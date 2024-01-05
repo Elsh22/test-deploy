5c71b504fcc25946b5255f5bd54e491fa6ddd41d
Elsh22 <aelshowaya@gmail.com>
2024-01-05 13:13:12 -0500

    first commit

--- sst.config.ts
+++ sst.config.ts
@@ -10,8 +10,14 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
-
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+        MONGODB_URI: process.env.MONGODB_URI || "your_default_mongodb_uri",
+        MONGODB_Signin_URI: process.env.MONGODB_SIGNIN_URI || "your_default_signin_uri",
+        TOKEN_SECRET: process.env.TOKEN_SECRET || "your_default_token_secret",
+        DOMAIN: process.env.DOMAIN || "your_default_domain",
+        },
+      });
       stack.addOutputs({
         SiteUrl: site.url,
       });
