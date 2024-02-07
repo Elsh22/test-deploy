535f617ba2a23efb34f3b2223b2113f5e729f67e
Elsh22 <aelshowaya@gmail.com>
2024-02-06 21:08:21 -0500

    GBM 02/07 Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com'],
-      },
-      webpack: (config) => {
-        config.externals = [...config.externals, 'bcrypt'];
-        return config;
-      }
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 };
 
 module.exports = nextConfig
