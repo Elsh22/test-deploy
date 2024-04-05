90b22d7f5ad9bc13d0466b13272fab3cdeb81a39
Elsh22 <aelshowaya@gmail.com>
2024-04-05 11:23:01 -0400

    Update Donor.jsx

--- src/components/Donor/Donor.jsx
+++ src/components/Donor/Donor.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Donor = () => {
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover donor-img ">
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover donor-img bg-blend-darken">
       <div>
         <h2 className="text-5xl font-bold text-black ">Join Our Mission to Make a Difference</h2>
         <p className="py-5 text-xl text-black font-bold">Empower the next generation of leaders with your support. DMC stands at the forefront of fostering a nurturing environment for men of color in the college setting. </p>
