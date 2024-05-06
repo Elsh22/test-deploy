3f0375c541d1dd74251fdfdcd3cb9c31e0466fdf
Elsh22 <aelshowaya@gmail.com>
2024-05-06 17:28:00 -0400

    update background image
    background image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,16 +6,16 @@ import Button from './Button';
 //bg-black/50
 const Home  = ({ 
   id, 
-  heading = 'Developing <br/> Men of <br/> Color', 
+  heading = 'Developing Men of Color', 
   message = 'At Virginia Commonwealth University',
   message2 = 'DMC is Offering a Scholarships Click here to learn more'
 }) => {
   return (
-    <div id={id} className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
-      <div className='p-5 text-white z-[2] mt-[-10rem]'>
-        <h2 className='text-5xl font-bold' dangerouslySetInnerHTML={{ __html: heading }}></h2>
-        <p className='py-5 text-xl'>{message}</p>
+    <div id={id} className='flex items-center justify-center h-screen  bg-fixed bg-center bg-cover custom-img'>
+      <div className='absolute top-0 left-0 right-0 bottom-0 z-[2]' />
+      <div className='p-5 text-white z-[2] mt-20'>
+        <h2 className='text-4xl font-bold' dangerouslySetInnerHTML={{ __html: heading }}></h2>
+        <p className='py-5 text-xl '>{message}</p>
         <ButtonClientComponent />
       </div>
     </div>
