813c2558b4f8cec7713fa4afe151fb2fc556188e
elnafem <117880199+elnafem@users.noreply.github.com>
2024-09-28 14:49:54 -0400

    Update global css and mission

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { TypingText } from '../../utils/CustomText';
-import IMG1 from '../../newassest/DevelopingMenofColor.png'; // Ensure this import is correct
+import IMG1 from '../../newassest/Ourmissionpicture.jpg'; // Ensure this import is correct
 import styles from '../../styles/index';
 import { fadeIn, staggerContainer, planetVariants } from '../../utils/motion';
 
@@ -11,11 +11,16 @@ const Mission = ({ id }) => {
       // Adjusted for responsiveness: stacks vertically on small screens, horizontally on larger screens
       <div className="flex flex-col md:flex-row items-center m-5 gap-4 md:gap-8" id={id}>
         <section className="w-full md:flex-1">
-          <TypingText title="DMC Leadership" textStyles="text-center"  />
+         <TypingText title="DMC Leadership" textStyles="text-center"  /> 
           <motion.div
             variants={planetVariants('left')} // Ensure you have defined or adjusted the planetVariants function
             className={`${styles.flexCenter} mb-4 md:mb-0`} // Added marginBottom for non-md screens
           >
+            <img
+              src={IMG1.src}
+              alt="Developing Men of Color"
+              className="w-[80%] md:w-[90%] h-auto object-contain" // Adjusted for responsiveness
+            ></img>
             
           </motion.div>
         </section>
@@ -27,7 +32,7 @@ const Mission = ({ id }) => {
               viewport={{ once: false, amount: 0.25 }}
               className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
           >
-            <TypingText title="Our Mission" textStyles="text-center" />
+           <TypingText title="Our Mission" textStyles="text-center"/>
             <motion.p
             variants={fadeIn('up', 'tween', 0.2, 1)}
             className="mt-4 md:mt-[8px] font-normal sm:text-[32px] md:text-[20px] text-center text-secondary-black overflow-x-hidden" // Adjusted for responsiveness
@@ -43,7 +48,7 @@ const Mission = ({ id }) => {
 
 export default Mission;
 /*<img
-              /*src={IMG1.src}
+              src={IMG1.src}
               alt="Developing Men of Color"
               className="w-[80%] md:w-[90%] h-auto object-contain" // Adjusted for responsiveness
             >*/
