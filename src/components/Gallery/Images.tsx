247db61fd6e65a1c41630e161cb42044c6c95b47
elnafem <117880199+elnafem@users.noreply.github.com>
2024-10-06 18:58:11 -0400

    fixed gallery

--- src/components/Gallery/Images.tsx
+++ src/components/Gallery/Images.tsx
@@ -30,137 +30,271 @@ import img41 from "../../newassest/sports3.jpg";
 import img42 from "../../newassest/sports4.jpg";
 import img43 from "../../newassest/sports5.jpg";
 import img44 from "../../newassest/sports6.jpg";
+import img45 from "../../newassest/DSC01289-Enhanced-NR.jpg";
+import img46 from "../../newassest/DSC02839.jpg";
+import img47 from "../../newassest/DSC02815.jpg";
+import img48 from "../../newassest/DSC02781.jpg";
+import img49 from "../../newassest/MG0033.jpg";
+import img50 from "../../newassest/MG0012.jpg";
+import img51 from "../../newassest/MG0004.jpg";
+import img52 from "../../newassest/Mixer-pic.jpg";
+import img53 from "../../newassest/DSC01313.jpg";
+import img54 from "../../newassest/DSC01295-Enhanced-NR.jpg";
+import img55 from "../../newassest/MG0004.jpg";
+import img56 from "../../newassest/DSC09876.jpg";
+import img57 from "../../newassest/DSC09585.jpg";
+import img58 from "../../newassest/DSC09921.jpg";
+import img59 from "../../newassest/DSC01103-Enhanced-NR.jpg";
+import img60 from "../../newassest/DSC01124-Enhanced-NR.jpg";
+import img61 from "../../newassest/DSC01289-Enhanced-NR.jpg";
+import img62 from "../../newassest/DSC01295-Enhanced-NR.jpg";
+import img63 from "../../newassest/DSC01313.jpg";
+import img64 from "../../newassest/DSC02781.jpg";
+import img65 from "../../newassest/DSC01629-Enhanced-NR.jpg";
+import img66 from "../../newassest/MG0012.jpg";
+import img67 from "../../newassest/DSC01630-Enhanced-NR.jpg";
+import img68 from "../../newassest/DSC01636-Enhanced-NR.jpg";
+import img69 from "../../newassest/DSC01614-Enhanced-NR.jpg";
+import img70 from "../../newassest/DSC01604-Enhanced-NR.jpg";
+import img71 from "../../newassest/DSC01590-Enhanced-NR.jpg";
 
 const imageData = [
-    {
-      id: 2,
-      imgSrc: img2,
-    },
-    {
-      id: 3,
-      imgSrc: img3,
-    },
-    {
-      id: 15,
-      imgSrc: img15,
-    },
-    {
-      id: 16,
-      imgSrc: img16,
-    },
-    {
-      id: 17,
-      imgSrc: img17,
-    },
-    {
-      id: 18,
-      imgSrc: img18,
-    },
-    {
-      id: 19,
-      imgSrc: img19,
-    },
-    {
-      id: 20,
-      imgSrc: img20,
-    },
-    {
-      id: 21,
-      imgSrc: img21,
-    },
-    {
-      id: 22,
-      imgSrc: img22,
-    },
-    {
-      id: 23,
-      imgSrc: img23,
-    },
-    {
-      id: 24,
-      imgSrc: img24,
-    },
-    {
-      id: 25,
-      imgSrc: img25,
-    },
-    {
-      id: 26,
-      imgSrc: img26,
-    },
-    {
-      id: 27,
-      imgSrc: img27,
-    },
-    {
-      id: 28,
-      imgSrc: img28,
-    },
-    {
-      id: 29,
-      imgSrc: img29,
-    },
-    {
-      id: 30,
-      imgSrc: img30,
-    },
-    {
-      id: 31,
-      imgSrc: img31,
-    },
-    {
-      id: 32,
-      imgSrc: img32,
-    },
-    {
-      id: 33,
-      imgSrc: img33,
-    },
-    {
-      id: 34,
-      imgSrc: img34,
-    },
-    {
-      id: 35,
-      imgSrc: img35,
-    },
-    {
-      id: 36,
-      imgSrc: img36,
-    },
-    {
-      id: 37,
-      imgSrc: img37,
-    },
-    {
-      id: 38,
-      imgSrc: img38,
-    },
-    {
-      id: 39,
-      imgSrc: img39,
-    },
-    {
-      id: 40,
-      imgSrc: img40,
-    },
-    {
-      id: 41,
-      imgSrc: img41,
-    },
-    {
-      id: 42,
-      imgSrc: img42,
-    },
-    {
-      id: 43,
-      imgSrc: img43,
-    },
-    {
-      id: 44,
-      imgSrc: img44,
-    },
-    
-  ];
+  {
+    id: 71,
+    imgSrc: img71,
+  },
+  {
+    id: 70,
+    imgSrc: img70,
+  },
+  {
+    id: 69,
+    imgSrc: img69,
+  },
+  {
+    id: 68,
+    imgSrc: img68,
+  },
+  {
+    id: 67,
+    imgSrc: img67,
+  },
+  {
+    id: 66,
+    imgSrc: img66,
+  },
+  {
+    id: 65,
+    imgSrc: img65,
+  },
+  {
+    id: 64,
+    imgSrc: img64,
+  },
+  {
+    id: 63,
+    imgSrc: img63,
+  },
+  {
+    id: 62,
+    imgSrc: img62,
+  },
+  {
+    id: 61,
+    imgSrc: img61,
+  },
+  {
+    id: 60,
+    imgSrc: img60,
+  },
+  {
+    id: 59,
+    imgSrc: img59,
+  },
+  {
+    id: 58,
+    imgSrc: img58,
+  },
+  {
+    id: 57,
+    imgSrc: img57,
+  },
+  {
+    id: 56,
+    imgSrc: img56,
+  },
+  {
+    id: 55,
+    imgSrc: img55,
+  },
+  {
+    id: 54,
+    imgSrc: img54,
+  },
+  {
+    id: 53,
+    imgSrc: img53,
+  },
+  {
+    id: 52,
+    imgSrc: img52,
+  },
+  {
+    id: 51,
+    imgSrc: img51,
+  },
+  {
+    id: 50,
+    imgSrc: img50,
+  },
+  {
+    id: 49,
+    imgSrc: img49,
+  },
+  {
+    id: 48,
+    imgSrc: img48,
+  },
+  {
+    id: 47,
+    imgSrc: img47,
+  },
+  {
+    id: 46,
+    imgSrc: img46,
+  },
+  {
+    id: 45,
+    imgSrc: img45,
+  },
+  {
+    id: 44,
+    imgSrc: img44,
+  },
+  {
+    id: 43,
+    imgSrc: img43,
+  },
+  {
+    id: 42,
+    imgSrc: img42,
+  },
+  {
+    id: 41,
+    imgSrc: img41,
+  },
+  {
+    id: 40,
+    imgSrc: img40,
+  },
+  {
+    id: 39,
+    imgSrc: img39,
+  },
+  {
+    id: 38,
+    imgSrc: img38,
+  },
+  {
+    id: 37,
+    imgSrc: img37,
+  },
+  {
+    id: 36,
+    imgSrc: img36,
+  },
+  {
+    id: 35,
+    imgSrc: img35,
+  },
+  {
+    id: 34,
+    imgSrc: img34,
+  },
+  {
+    id: 33,
+    imgSrc: img33,
+  },
+  {
+    id: 32,
+    imgSrc: img32,
+  },
+  {
+    id: 31,
+    imgSrc: img31,
+  },
+  {
+    id: 30,
+    imgSrc: img30,
+  },
+  {
+    id: 29,
+    imgSrc: img29,
+  },
+  {
+    id: 28,
+    imgSrc: img28,
+  },
+  {
+    id: 27,
+    imgSrc: img27,
+  },
+  {
+    id: 26,
+    imgSrc: img26,
+  },
+  {
+    id: 25,
+    imgSrc: img25,
+  },
+  {
+    id: 24,
+    imgSrc: img24,
+  },
+  {
+    id: 23,
+    imgSrc: img23,
+  },
+  {
+    id: 22,
+    imgSrc: img22,
+  },
+  {
+    id: 21,
+    imgSrc: img21,
+  },
+  {
+    id: 20,
+    imgSrc: img20,
+  },
+  {
+    id: 19,
+    imgSrc: img19,
+  },
+  {
+    id: 18,
+    imgSrc: img18,
+  },
+  {
+    id: 17,
+    imgSrc: img17,
+  },
+  {
+    id: 16,
+    imgSrc: img16,
+  },
+  {
+    id: 15,
+    imgSrc: img15,
+  },
+  {
+    id: 3,
+    imgSrc: img3,
+  },
+  {
+    id: 2,
+    imgSrc: img2,
+  },
+];
 
-  export default imageData;
+export default imageData;
