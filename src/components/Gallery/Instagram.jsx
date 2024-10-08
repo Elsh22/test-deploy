8ac142c38fb02c4fcdb7616a5740b8bd1f2cd255
Elsh22 <76716965+Elsh22@users.noreply.github.com>
2024-10-08 16:21:26 -0400

    Update Instagram.jsx

--- src/components/Gallery/Instagram.jsx
+++ src/components/Gallery/Instagram.jsx
@@ -7,21 +7,20 @@ class InstagramFeed extends Component {
       get: 'user',
       target: 'instafeed-container',
       resolution: 'standard_resolution',
-      accessToken: 'IGQWRPLV8tOFhHTzQtTWhJdWxaS2I0bk1UZAkRrOUxYSTh2TVhseTR3MWxoT1Rid3hRQzNzX3Y5Y1hEYmN6Y2h1RU9falhDSzJjalR6cVNNX1djN09jNXYya2hoeDJ6LUQySDJQRDA4NXpJcWVfd0xscDlvR3NKdk0ZD', // Updated line
+      accessToken: process.env.NEXT_PUBLIC_INSTAGRAM_ACCESS_TOKEN, // Updated line
       template: `<a href="{{link}}" class="block overflow-hidden mb-3">
-                      <div class="relative" style="padding-bottom: 100%;"> <!-- Aspect ratio box -->
-                        <img src="{{image}}" class="absolute top-0 left-0 w-full h-full object-cover" />
-                      </div>
-                   </a>`
+                    <div class="relative" style="padding-bottom: 100%;"> <!-- Aspect ratio box -->
+                      <img src="{{image}}" class="absolute top-0 left-0 w-full h-full object-cover" />
+                    </div>
+                 </a>`
     });
     userFeed.run();
   }
-  
-  
-    render() {
-      return (
-        <div className="mt-4">
-            <div id="instafeed-container" className="gallery grid grid-cols-3 gap-3 md:grid-cols-2 sm:grid-cols-1 p-3"></div>
+
+  render() {
+    return (
+      <div className="mt-4">
+        <div id="instafeed-container" className="gallery grid grid-cols-3 gap-3 md:grid-cols-2 sm:grid-cols-1 p-3"></div>
       </div>
     );
   }
