8b0785307aff34dc419351ec1ff745f1b0ce8da0
elnafem <117880199+elnafem@users.noreply.github.com>
2024-11-21 11:48:55 -0500

    Update Highlight.jsx

--- src/components/Highlights/Highlight.jsx
+++ src/components/Highlights/Highlight.jsx
@@ -8,7 +8,7 @@ import img4 from '../../newassest/DMCGettingCheckfromVACU.jpg';
 import img5 from '../../newassest/darrellgettinglinkedin.jpg';
 
 const events = [
-    { id: 1, title: 'Jacobs Chances', imgSrc: img1 },
+    { id: 1, title: 'Jacobs Chance`s', imgSrc: img1 },
     { id: 2, title: 'College Day', imgSrc: img2 },
     { id: 3, title: 'Resource Fair', imgSrc: img3 },
     { id: 4, title: 'General Body Meeting With Virginia Credit Union', imgSrc: img4 },
