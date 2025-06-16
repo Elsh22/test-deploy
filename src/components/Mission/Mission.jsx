8615168a7ffbcc41b22a1372c50d22caa8ec7147
Elsh22 <76716965+Elsh22@users.noreply.github.com>
2025-06-16 03:22:01 +0000

    if i get this error one more time...

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -103,7 +103,7 @@ const Mission = ({ id }) => {
                 variants={fadeIn('up', 'tween', 0.2, 1)}
                 className="heading-dmc-primary text-dmc-h1 leading-tight"
               >
-                Empowering Tomorrow's
+                Empowering Tomorrow&aposs
                 <span className="text-dmc-gradient block">Leaders</span>
               </motion.h2>
               
